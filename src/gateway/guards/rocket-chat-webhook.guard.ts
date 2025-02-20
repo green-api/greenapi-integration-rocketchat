fc6d30ec6211880f3673722e25304edd425cafb7
SomedayIsTheDay <somedayistheday1@gmail.com>
2025-02-20 12:12:49 +0500

    chore: update guard logs

--- src/gateway/guards/rocket-chat-webhook.guard.ts
+++ src/gateway/guards/rocket-chat-webhook.guard.ts
@@ -19,32 +19,47 @@ export class RocketChatWebhookGuard implements CanActivate {
 		const request = context.switchToHttp().getRequest();
 		const token = request.headers["x-rocketchat-livechat-token"];
 		if (!token) {
-			this.logger.warn("X-Rocketchat-Livechat-Token header is missing", request.body);
+			this.logger.warn("X-Rocketchat-Livechat-Token header is missing", {
+				body: request.body,
+				headers: request.headers,
+			});
 			throw new HttpException({message: "X-Rocketchat-Livechat-Token header is missing"}, HttpStatus.OK);
 		}
 
 		const message = request.body as RocketChatWebhook;
 
 		if (!message.messages?.[0]?.agentId) {
-			this.logger.warn("Not an agent message", message);
+			this.logger.warn("Not an agent message", {
+				body: request.body,
+				headers: request.headers,
+			});
 			throw new HttpException({message: "Not an agent message"}, HttpStatus.OK);
 		}
 
 		const agentEmail = message?.agent?.email;
 		if (!agentEmail) {
-			this.logger.warn("Invalid webhook format", message);
+			this.logger.warn("Invalid webhook format", {
+				body: request.body,
+				headers: request.headers,
+			});
 			throw new HttpException({message: "Invalid webhook format"}, HttpStatus.OK);
 		}
 
 		const user = await this.db.findUser(agentEmail);
 		if (!user) {
-			this.logger.warn("No user with such credentials", message);
+			this.logger.warn("No user with such credentials", {
+				body: request.body,
+				headers: request.headers,
+			});
 			throw new HttpException({message: "No user with such credentials"}, HttpStatus.OK);
 		}
 
 		const workspace = await this.db.findWorkspaceById(user.workspaceId);
 		if (!workspace || workspace.webhookToken !== token) {
-			this.logger.warn("Invalid webhook token", message);
+			this.logger.warn("Invalid webhook token", {
+				body: request.body,
+				headers: request.headers,
+			});
 			throw new HttpException({message: "Invalid webhook token"}, HttpStatus.OK);
 		}
 
@@ -52,8 +67,11 @@ export class RocketChatWebhookGuard implements CanActivate {
 			`${message.visitor.token.split(":")[1]}@c.us`, workspace.id,
 		);
 		if (!instance) {
-			this.logger.warn("Instance by phone number is not found", message);
-			throw new HttpException({message: "Instance by phone number is not found"}, HttpStatus.OK);
+			this.logger.warn("Instance by phone number is not found or the instance unexpectedly changed its state", {
+				body: request.body,
+				headers: request.headers,
+			});
+			throw new HttpException({message: "Instance by phone number is not found or the instance unexpectedly changed its state"}, HttpStatus.OK);
 		}
 
 		request.instance = instance;
